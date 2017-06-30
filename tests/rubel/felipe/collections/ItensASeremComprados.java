29c189afd3ab5a59715136eb5c540d6ebf66029d
Miere <miere00@gmail.com>
2017-06-29 22:12:18 -0300

    Working with Lists and Maps

---
+++ tests/rubel/felipe/collections/ItensASeremComprados.java
@@ -0,0 +1,15 @@
+package rubel.felipe.collections;
+
+import lombok.*;
+
+/**
+ *
+ */
+@ToString
+@AllArgsConstructor
+public class ItensASeremComprados {
+
+	String nome;
+	double preço;
+	String categoria;
+}
