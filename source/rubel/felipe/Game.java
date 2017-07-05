98d46834a746559bde6b402c17c845b42680833f
Felipe R. Borges <felipe.rubel@hotmail.com>
2017-07-04 21:04:05 -0300

    Upload dos exercicios prontos

--- source/rubel/felipe/Game.java
+++ source/rubel/felipe/Game.java
@@ -2,63 +2,8 @@ package rubel.felipe;
 
 public class Game {
 
-	static boolean emJogo = true;
-
 	public static void main(String[] args) {
 
-		atacarInimigo();
-		atacarZiska();
-		atacarCitizen();
-
-	}
-
-	public static void atacarInimigo() {
-		Character enemy = new Character();
-
-		enemy.setHealth(100);
-		Weapon weapon = new Weapon(39);
-		weapon.hurt(enemy);
-
-		if (enemy.health < 0) {
-
-			System.out.println("Ziska Matou um Inimigo, Você ganhou o jogo");
-
-			emJogo = false;
-
-		}
-
-	}
-
-	public static void atacarZiska() {
-		Character ziska = new Character();
-
-		ziska.setHealth(100);
-		Weapon weapon = new Weapon(59);
-		weapon.hurt(ziska);
-
-		if (ziska.health < 0) {
-
-			System.out.println("Ziska Matou um Inimigo, Você ganhou o jogo");
-
-			emJogo = false;
-
-		}
-	}
-
-	public static void atacarCitizen() {
-		Character citizen = new Character();
-
-		citizen.setHealth(100);
-		Weapon weapon = new Weapon(39);
-		weapon.hurt(citizen);
-
-		if (citizen.health < 0) {
-
-			System.out.println("Ziska Matou um Cidadão, o jogo foi perdido");
-
-			emJogo = false;
-
-		}
 	}
 
 }
--- tests/rubel/felipe/collections/TrabalhandoComListas.java
+++ tests/rubel/felipe/collections/TrabalhandoComListas.java
@@ -1,89 +1,141 @@
 package rubel.felipe.collections;
 
 import static org.junit.Assert.assertEquals;
+
+import java.math.BigDecimal;
 import java.util.*;
+import java.util.Map.Entry;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
 import org.junit.*;
 
 /**
- *
- */
+*
+*/
 public class TrabalhandoComListas {
 
 	ArrayList<ItensASeremComprados> itens;
 
 	@Before
-	public void criarItensASeremComprados(){
+	public void criarItensASeremComprados() {
 		itens = new ArrayList<>();
-		itens.add( new ItensASeremComprados( "Arroz", 5.99, "Alimento" ) );
-		itens.add( new ItensASeremComprados( "Feijao", 10.99, "Alimento" ) );
-		itens.add( new ItensASeremComprados( "Macarrao", 3.45, "ALimento" ) );
-		itens.add( new ItensASeremComprados( "Cerveja", 3.00, "Bebidas" ) );
-		itens.add( new ItensASeremComprados( "Cerveja Ruim", 1.70, "Bebidas" ) );
-		itens.add( new ItensASeremComprados( "Pneu", 120.00, "Transporte" ) );
-		itens.add( new ItensASeremComprados( "Fraldas", 55.00, "Crianças" ) );
+		itens.add(new ItensASeremComprados("Arroz", 5.99, "Alimento"));
+		itens.add(new ItensASeremComprados("Feijao", 10.99, "Alimento"));
+		itens.add(new ItensASeremComprados("Macarrao", 3.45, "ALimento"));
+		itens.add(new ItensASeremComprados("Cerveja", 3.00, "Bebidas"));
+		itens.add(new ItensASeremComprados("Cerveja Ruim", 1.70, "Bebidas"));
+		itens.add(new ItensASeremComprados("Pneu", 120.00, "Transporte"));
+		itens.add(new ItensASeremComprados("Fraldas", 55.00, "Crianças"));
 	}
 
 	/**
-	 * Ordene a lista por preço.
-	 * Restriçao: com 4 ou menos linhas.
+	 * Ordene a lista por preço. Restriçao: com 4 ou menos linhas.
 	 */
 	@Test
-	public void exercicio1(){
+	public void exercicio1() {
 
+		Collections.sort(itens,
+				(ItensASeremComprados o1, ItensASeremComprados o2) -> Double.compare(o1.preço, o2.preço));
 
-		assertEquals( 1.70, itens.get( 0 ).preço, 0 );
-		assertEquals( 3.00, itens.get( 1 ).preço, 0 );
-		assertEquals( 3.45, itens.get( 2 ).preço, 0 );
-		assertEquals( 5.99, itens.get( 3 ).preço, 0 );
-		assertEquals( 10.99, itens.get( 4 ).preço, 0 );
-		assertEquals( 55.00, itens.get( 5 ).preço, 0 );
-		assertEquals( 120.00, itens.get( 6 ).preço, 0 );
+		// ** Outra forma de realizar o exercicio **
+		/*
+		 * Collections.sort(itens, new Comparator<ItensASeremComprados>() {
+		 * 
+		 * @Override public int compare(ItensASeremComprados o1,
+		 * ItensASeremComprados o2) {
+		 * 
+		 * return Double.compare(o1.preço, o2.preço);
+		 * 
+		 * } });
+		 */
+
+		assertEquals(1.70, itens.get(0).preço, 0);
+		assertEquals(3.00, itens.get(1).preço, 0);
+		assertEquals(3.45, itens.get(2).preço, 0);
+		assertEquals(5.99, itens.get(3).preço, 0);
+		assertEquals(10.99, itens.get(4).preço, 0);
+		assertEquals(55.00, itens.get(5).preço, 0);
+		assertEquals(120.00, itens.get(6).preço, 0);
+
 	}
 
 	/**
-	 * Ordene a lista alfabeticamente por nome.
-	 * Restriçao: com 4 ou menos linhas.
+	 * Ordene a lista alfabeticamente por nome. Restriçao: com 4 ou menos
+	 * linhas.
 	 */
 	@Test
-	public void exercicio2(){
+	public void exercicio2() {
 
+		Collections.sort(itens, (ItensASeremComprados o1, ItensASeremComprados o2) -> o1.nome.compareTo(o2.nome));
 
-		assertEquals( "Arroz", itens.get( 0 ).nome );
-		assertEquals( "Cerveja", itens.get( 1 ).nome );
-		assertEquals( "Cerveja Ruim", itens.get( 2 ).nome );
-		assertEquals( "Feijao", itens.get( 3 ).nome );
-		assertEquals( "Fraldas", itens.get( 4 ).nome );
-		assertEquals( "Macarrao", itens.get( 5 ).nome );
-		assertEquals( "Pneu", itens.get( 6 ).nome );
+		// ** Outra forma de realizar o exercicio **
+		/*
+		 * Collections.sort(itens, new Comparator<ItensASeremComprados>() {
+		 * 
+		 * @Override public int compare(ItensASeremComprados o1,
+		 * ItensASeremComprados o2) {
+		 * 
+		 * return o1.nome.compareTo(o2.nome);
+		 * 
+		 * } });
+		 */
+
+		assertEquals("Arroz", itens.get(0).nome);
+		assertEquals("Cerveja", itens.get(1).nome);
+		assertEquals("Cerveja Ruim", itens.get(2).nome);
+		assertEquals("Feijao", itens.get(3).nome);
+		assertEquals("Fraldas", itens.get(4).nome);
+		assertEquals("Macarrao", itens.get(5).nome);
+		assertEquals("Pneu", itens.get(6).nome);
 	}
 
 	/**
-	 * Classificar itens por "Categoria".
-	 * Restriçao: com 8 ou menos linhas.
+	 * Classificar itens por "Categoria". Restriçao: com 8 ou menos linhas.
 	 */
 	@Test
-	public void exercicio3()
-	{
-		HashMap<String, ArrayList<ItensASeremComprados>> itensClassificados = null;
+	public void exercicio3() {
+		Map<String, List<ItensASeremComprados>> itensClassificados;
 
-		assertEquals( 3, itensClassificados.get("Alimento").size() );
-		assertEquals( 2, itensClassificados.get("Bebidas").size() );
-		assertEquals( 1, itensClassificados.get("Crianças").size() );
-		assertEquals( 1, itensClassificados.get("Transporte").size() );
+		ItensASeremComprados itens[] = { new ItensASeremComprados("Arroz", 5.99, "Alimento"),
+										 new ItensASeremComprados("Feijao", 10.99, "Alimento"),
+										 new ItensASeremComprados("Macarrao", 3.45, "Alimento"),
+										 new ItensASeremComprados("Cerveja", 3.00, "Bebidas"),
+										 new ItensASeremComprados("Cerveja Ruim", 1.70, "Bebidas"),
+										 new ItensASeremComprados("Pneu", 120.00, "Transporte"),
+										 new ItensASeremComprados("Fraldas", 55.00, "Crianças") };
+
+		itensClassificados = Arrays.asList(itens).stream().collect(Collectors.groupingBy(i -> i.categoria));
+
+		assertEquals(3, itensClassificados.get("Alimento").size());
+		assertEquals(2, itensClassificados.get("Bebidas").size());
+		assertEquals(1, itensClassificados.get("Crianças").size());
+		assertEquals(1, itensClassificados.get("Transporte").size());
 	}
 
 	/**
-	 * Gerar um relatorio de Categoria e a soma do preço de todos os produtos a ela associados.
-	 * Restriçao: com 8 ou menos linhas.
+	 * Gerar um relatorio de Categoria e a soma do preço de todos os produtos a
+	 * ela associados. Restriçao: com 8 ou menos linhas.
 	 */
 	@Test
-	public void exercicio4(){
-		HashMap<String, Double> itensClassificados = null;
+	public void exercicio4() {
+		Map<String, Double> itensClassificados = null;
 
-		assertEquals( 20.43, itensClassificados.get("Alimento"), 0 );
-		assertEquals( 6.45, itensClassificados.get("Bebidas"), 0 );
-		assertEquals( 55.00, itensClassificados.get("Crianças"), 0 );
-		assertEquals( 120.00, itensClassificados.get("Transporte"), 0 );
+		ItensASeremComprados itens[] = { new ItensASeremComprados("Arroz", 5.99, "Alimento"),
+										 new ItensASeremComprados("Feijao", 10.99, "Alimento"),
+										 new ItensASeremComprados("Macarrao", 3.45, "Alimento"),
+										 new ItensASeremComprados("Cerveja", 3.00, "Bebidas"),
+										 new ItensASeremComprados("Cerveja Ruim", 1.70, "Bebidas"),
+										 new ItensASeremComprados("Pneu", 120.00, "Transporte"),
+										 new ItensASeremComprados("Fraldas", 55.00, "Crianças") };
+
+		itensClassificados = Arrays.asList(itens).stream()
+				.collect(Collectors.groupingBy(i -> i.categoria, Collectors.summingDouble(j -> j.preço)));
+
+		assertEquals(20.43, itensClassificados.get("Alimento"), 0);
+		assertEquals(4.70, itensClassificados.get("Bebidas"), 0);
+		assertEquals(55.00, itensClassificados.get("Crianças"), 0);
+		assertEquals(120.00, itensClassificados.get("Transporte"), 0);
 	}
 
 }
