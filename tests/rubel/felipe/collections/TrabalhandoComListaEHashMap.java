9e0bb320f254524bf237978fa2044ee10f33b782
Felipe R. Borges <felipe.rubel@hotmail.com>
2017-07-20 21:39:44 -0300

    Atualizações exercicios.

--- tests/rubel/felipe/collections/TrabalhandoComListaEHashMap.java
+++ tests/rubel/felipe/collections/TrabalhandoComListaEHashMap.java
@@ -9,50 +9,53 @@ import org.junit.Test;
  */
 public class TrabalhandoComListaEHashMap {
 
-	List<Integer> numeros = Arrays.asList( 1, 1, 2, 3 );
+	List<Integer> numeros = Arrays.asList(1, 1, 2, 3);
 
 	@Test
-	public void exercicio0(){
+	public void exercicio0() {
 		Integer total = somarNumeros();
-		assertEquals( 7, total, 0 );
+		assertEquals(7, total, 0);
 	}
 
 	private Integer somarNumeros() {
 
 		int soma = 0;
 
-		for(Integer numero: numeros)
-		{
+		for (Integer numero : numeros) {
 			soma = numero + soma;
 		}
 
 		return soma;
 	}
 
-
-
-
-
-
-
 	@Test
-	public void exercicio1(){
+	public void exercicio1() {
 		Map<Integer, Integer> totais = contarQuantidadeDosNumeros();
 
-		assertEquals( 2, totais.get( 1 ), 0 );
-		assertEquals( 1, totais.get( 2 ), 0 );
-		assertEquals( 1, totais.get( 3 ), 0 );
+		assertEquals(2, totais.get(1), 0);
+		assertEquals(1, totais.get(2), 0);
+		assertEquals(1, totais.get(3), 0);
 	}
 
 	private Map<Integer, Integer> contarQuantidadeDosNumeros() {
 		Map<Integer, Integer> totais = new HashMap<>();
 
+		int contador = 0;
 
-		for(Integer numero:numeros){
+		for (Integer numero : numeros) {
 
-			
+			if (totais.containsKey(numero)) {
+
+				contador = totais.get(numero);
+				contador++;
+
+				totais.put(numero, contador);
+			} else {
+				totais.put(numero, 1);
+			}
 
 		}
+		System.out.println(totais);
 
 		return totais;
 	}
